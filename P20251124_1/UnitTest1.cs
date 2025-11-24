cee69548eeed969ee409dc56a0d459a0db4e6b93
kannar <adamkannar2008@gmail.com>
2025-11-24 14:01:45 +0100

    a

--- P20251124_1/UnitTest1.cs
+++ P20251124_1/UnitTest1.cs
@@ -1,17 +1,22 @@
-﻿namespace P20251124_1
+﻿using NUnit.Framework.Legacy;
+
+namespace P20251124_1
 {
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
-            int x=0; int y=0;
+            int x=0; 
+            int y=0;
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            int x = 5;
+            string szoveg = "premontrei";
+            ClassicAssert.NotZero(x);
         }
     }
 }
