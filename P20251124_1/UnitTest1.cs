6f3dc43b6d9549cfe7792c890cc2acb7077cfb9d
kannar <adamkannar2008@gmail.com>
2025-11-24 12:59:30 +0100

    Add project files.

---
+++ P20251124_1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace P20251124_1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
